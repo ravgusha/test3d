9d3c02ddf8d6fb6a09a279e2d61535da1c9f1287
ravgusha <ravgusha@gmail.com>
2023-03-17 17:20:27 +0300

    refactor: refactor range slider

--- src/index.js
+++ src/index.js
@@ -4,3 +4,6 @@ import './fonts/fonts.scss';
 if (process.env.NODE_ENV !== 'production') {
 	require('./index.html');
 }
+document.querySelector('.slider').onchange = function(e){
+	this.style.background = 'linear-gradient(to right, #FF69B4, #FF69B4 ' + this.value + '%, #5BAAF9 ' + this.value + '%)';
+  };
