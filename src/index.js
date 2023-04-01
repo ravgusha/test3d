c27778bdf8e3536edb1228840cf73493146555a6
ravgusha <ravgusha@gmail.com>
2023-04-01 12:43:37 +0300

    feat: add media queries

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './fonts/fonts.scss';
 if (process.env.NODE_ENV !== 'production') {
 	require('./index.html');
 }
-document.querySelector('.slider').onchange = function(e){
-	this.style.background = 'linear-gradient(to right, #FF69B4, #FF69B4 ' + this.value + '%, #5BAAF9 ' + this.value + '%)';
-  };
+
+document.querySelector('.slider').oninput = function (e) {
+	this.style.background =
+		'linear-gradient(to right, #FF69B4, #FF69B4 ' + this.value + '%, #5BAAF9 ' + this.value + '%)';
+};
